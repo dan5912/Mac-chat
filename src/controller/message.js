e7ddea146d070f7f6f55085cb48e06fa0114cbaf
Daniel Hill <danh5912@gmail.com>
2017-09-18 08:59:19 -0400

    Update message.js

--- src/controller/message.js
+++ src/controller/message.js
@@ -34,7 +34,7 @@ export default({ config, db }) => {
       if (err) {
         res.status(500).json({ message: err });
       }
-      res.status(200).json({ message: 'Message Successfully Removed'});
+      res.status(200).json({ message: message._id});
     });
   });
   
