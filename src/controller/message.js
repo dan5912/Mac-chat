0746a9fb34cdcaee58071773e18483e95477b881
Daniel Hill <danh5912@gmail.com>
2017-09-18 09:14:48 -0400

    Update message.js

--- src/controller/message.js
+++ src/controller/message.js
@@ -32,7 +32,7 @@ export default({ config, db }) => {
       if (err) {
         res.status(500).json({ message: err });
       }
-      res.status(200).json(message._id);
+      res.status(200).json(message._id.getTimestamp());
     });
   });
   
