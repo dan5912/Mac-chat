7d353234b316c1e7032ca716fd6e59fb22f468c1
Daniel Hill <danh5912@gmail.com>
2017-09-18 08:41:41 -0400

    Update message.js

--- src/model/message.js
+++ src/model/message.js
@@ -7,7 +7,7 @@ const ObjectId = mongoose.Schema.Types.ObjectId;
 var ObjectID = require('mongodb').ObjectID
 
 const messageSchema = new Schema({
-  _id: new ObjectID()
+  _id: new ObjectID(),
   messageBody: String, default: "",
   timeStamp: {type: Date, default: Date.now},
   userId: {type: ObjectId, ref: 'User'},
